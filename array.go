aee0a0637e526f50e5c24b1da2e8b757c41aa169
ryota.s.0810 <akaashi0810@gmail.com>
2021-06-28 13:36:34 +0900

    配列について

---
+++ array.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var a [5]int //a[0] - a[4]
+	// a[2] = 4
+	// a[4] = 23
+	// fmt.Println(len(a), cap(a))
+	// fmt.Println(a)
+	// fmt.Println(a[1], a[2])
+	// b := [3]int{1, 2, 3}
+	b := []int{1, 2, 3} //個数は省略できる
+	fmt.Println(b)
+}
