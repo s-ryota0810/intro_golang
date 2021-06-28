088613ae00e37803199728c9f14ead0e523e2bc0
ryota.s.0810 <akaashi0810@gmail.com>
2021-06-28 13:10:26 +0900

    演算について

---
+++ calc.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	//x = 10 % 3
+	x += 3
+	x++
+	fmt.Println(x)
+	var s string
+	s = "hello" + "world"
+	fmt.Println(s)
+
+	a, b := true, false
+	fmt.Println(a && b)
+	fmt.Println(a || b)
+	fmt.Println(!a)
+}
