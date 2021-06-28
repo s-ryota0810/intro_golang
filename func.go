0e6e26919144f058288a3957e4028b44e728b16d
ryota.s.0810 <akaashi0810@gmail.com>
2021-06-28 13:31:08 +0900

    関数その2

--- func.go
+++ func.go
@@ -1,14 +1,11 @@
 package main
 
-import "fmt"
-
 /*
 func hi(name string) string { //戻り値の型を指定する
 	// fmt.Println("hi!" + name)
 	msg := "hi!" + name
 	return msg
 }
-*/
 
 func hi(name string) (msg string) {
 	// fmt.Println("hi!" + name)
@@ -18,4 +15,20 @@ func hi(name string) (msg string) {
 func main() {
 	//hi("taguchi")
 	fmt.Println(hi("inoe"))
+}
+*/
+
+import "fmt"
+
+func main() {
+	//fmt.Println(swap(5, 2))
+	f := func(a, b int) (int, int) {
+		return b, a
+	}
+	fmt.Println(f(2, 3))
+	
+	//即時関数
+	func(msg string) {
+		fmt.Println(msg)
+	}("taguchi")
 }
