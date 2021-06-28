c426c532fa1d79b75f69e18d451e53c9a617816f
ryota.s.0810 <akaashi0810@gmail.com>
2021-06-28 12:57:56 +0900

    変数

--- hello.go
+++ hello.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	//var msg string
+	//msg = "Hello world"
+	//var msg = "Hello world"
+	msg := "Hello world"
+	fmt.Println(msg)
+
+	/*
+	var a, b int
+	a, b = 10, 14
+	a, b := 10, 14
+
+	var (
+		c int
+		d string
+	)
+	c = 20
+	d = "string"
+	*/
 }
